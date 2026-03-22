bdf87447f9500dcf09b4ec48a4127ec3736058dc
deksansaidd <dsaid@my.centennialcollege.ca>
2026-03-21 21:43:13 -0400

    Add Main class with basic webapp message

---
+++ src/main/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("comp367 webapp is running!");
+        Thread.currentThread().join();
+    }
+}
